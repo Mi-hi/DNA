16d2ec28179c10bfdb554a8ffa58f4ef7feaa823
Mi-hi <107835881+Mi-hi@users.noreply.github.com>
2023-07-21 11:09:42 -0400

    first commit

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,23 +1,18 @@
-
-
-#DNAToolkit file 
+#DNAToolkit file
 import collections
 Nucleotides = ['A', 'C', 'G', 'T']
 
-def validate_seq(dna_seq):
-    tmpseq = dna_seq.upper()
-    for nuc in tmpseq:
-        if nuc not in Nucleotides:
-            return False
-    return tmpseq
 
+def validate_seq(dna_seq):
+    Nucleotides = ['A', 'C', 'G', 'T']
+    for char in dna_seq:
+        if char.upper() not in Nucleotides:
+            return False
+    return dna_seq
 
 def countNucFreq(seq):
     #tmpFreqDict = {"A" : 0, "C" : 0, "G" : 0 , "T" : 0 }
     #for nuc in seq :
     #    tmpFreqDict[nuc] += 1
     #return tmpFreqDict
-        return dict(collections.Counter(seq))
-        
-
-    
+    return dict(collections.Counter(seq))
